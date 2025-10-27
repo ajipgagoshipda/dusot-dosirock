d90e1d0052756bac1e5ffa989bc4a5cb430d5f5c
ajipgagoshipda <jinramen9@gmail.com>
2025-10-27 20:14:35 +0900

    movie

--- ddivalll.py
+++ ddivalll.py
@@ -1,2 +1,25 @@
-print("집 가고싶다")
-print("월요일 캠프 밥 맛있는거 주면 좋겠어요")
+class Dog:
+    count = 0
+
+    def __init__(self, name):
+        self.name = name
+        Dog.count += 1
+
+    def bark(self):
+        print(f"{self.name}가 멍멍!하고 짖습니다.")
+
+    @classmethod
+    def show_count(cls):
+        print(f"현재 강아지수 {cls.count}")
+
+    @staticmethod
+    def sound():
+        print("개는 멍멍 소리를 냅니다.")
+
+dog1 = Dog("몽글이")
+dog2 = Dog("깽이")
+
+dog1.bark()
+dog2.bark()
+Dog.show_count()
+Dog.sound()
---
+++ movie.py
@@ -0,0 +1,68 @@
+import csv
+movies = []
+
+class movie:
+    count = 0
+
+    def __init__(self, title, duration, age):
+        self.title = title
+        self.duration  = duration
+        self.age = age
+        movie.count += 1
+
+    def display_info(self):
+        print(f"제목: {self.title}, 러닝타임: {self.duration}, 관람 가능 나이: {self.age}")
+
+    @classmethod
+    def show_count(cls):
+        print(f"현재 영화의 수: {cls.count}")
+    
+    @staticmethod
+    def now_showing():
+        print("현재 상영 중인 영화 입니다.")
+
+def load_movies(filename):
+    f = open(filename, "r", encoding="utf-8-sig")
+    reader = csv.reader(f)
+    
+    header = next(reader)
+    print(header)
+
+    for line in reader:
+        title, duration, age = line
+        movie_obj = movie(title, duration, age)
+        movies.append(movie_obj)
+
+    for m in movies:
+        m.display_info()
+    
+    movie.show_count()
+
+    f.close()
+
+def add_movie():
+    print("\n 새 영화를 추가합니다.")
+    title = input("영화 제목: ")
+    duration = int(input("러닝타임(분): "))
+    age = int(input("관람 가능 나이: "))
+
+    new_movie = movie(title, duration, age)
+    movies.append(new_movie)
+
+    print("\n 새 영화가 추가되었습니다!")
+    new_movie.display_info()
+
+def save_movie(filename):
+    f = open(filename, "w", newline="", encoding="utf-8-sig")
+    writer = csv.writer(f)
+    writer.wrirerow({"title", "duration", "age"})
+
+    for m in movies:
+        writer.writerow([m.title, m.duration, m.age])
+
+    print("파일이 업데이트 되었습니다.")
+    f.close()
+
+load_movies("movie.csv")
+add_movie()
+save_movie("movie.csv")
